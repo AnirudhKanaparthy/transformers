e72ebede22c3c4a4b666bb42afd5b8da08cf6f4f
Anirudh Kanaparthy <official.anirudh.kanaparthy@gmail.com>
2025-01-08 03:35:31 +0530

    added a way to get vocab size

--- tokenizers.py
+++ tokenizers.py
@@ -8,13 +8,16 @@ class TikTokenizer:
     def __init__(self, tokenizer_name: str):
         self._encoder = tiktoken.get_encoding(tokenizer_name)
     
-    def encode(self, x: Tensor, allowed_special: Union[set[str], None] = None):
+    def encode(self, x: Tensor, allowed_special: Union[set[str], None] = None) -> Tensor:
         if allowed_special is None:
             allowed_special = {"<|endoftext|>"}
         return self._encoder.encode(x, allowed_special=allowed_special)
 
-    def decode(self, x: Tensor):
+    def decode(self, x: Tensor) -> str:
         return self._encoder.decode(x)
+    
+    def vocabulary_size(self) -> int:
+        return self._encoder.max_token_value + 1
     
 
 class CharTokenizer:
@@ -22,8 +25,11 @@ class CharTokenizer:
         self._stoi = {ch: i for i, ch in enumerate(vocabulary)}
         self._itos = {i: ch for i, ch in enumerate(vocabulary)}
 
-    def encode(self, x: Tensor, _: Union[set[str], None] = None): 
+    def encode(self, x: Tensor, _: Union[set[str], None] = None) -> Tensor:
         return [self._stoi[ch] for ch in x]
     
-    def decode(self, x: Tensor): 
+    def decode(self, x: Tensor) -> str:
         return ''.join([self._itos[i] for i in x])
+    
+    def vocabulary_size(self) -> int:
+        return len(self._stoi)
